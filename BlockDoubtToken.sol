1e8802f64fcd1e2221b652179a20a379ea6647fe
CoRe103 <hiruma@geomerlin.com>
2018-10-02 23:25:29 +0900

    微修正

--- BlockDoubtToken.sol
+++ BlockDoubtToken.sol
@@ -1,16 +1,24 @@
 pragma solidity ^0.4.24;
 
-import "./ERC20/ERC20.sol";
+import "./ERC20.sol";
 
-//トークンのシンボルはBlockDoubtTokenでBDT
+// トークンのシンボルはBlockDoubtTokenでBDT
 contract BlockDoubtToken is ERC20 {
+
+    /*
+     * Storage
+     */
+
     string public name = "BlockDoubtToken";
     string public symbol = "BDT";
     uint public decimals = 0;
     address[] public judgemen;
-    
-    /// @return Returns index and ok of the first occurrence starting from index 0
-    function index(address[] addresses, address a) internal pure returns (uint, bool) {
+
+    /**
+     * @return Returns index and ok of the first occurrence starting from index 0
+     */
+
+    function index (address[] addresses, address a) internal pure returns (uint, bool) {
         for (uint i = 0; i < addresses.length; i++) {
             if (addresses[i] == a) {
                 return (i, true);
@@ -18,30 +26,42 @@ contract BlockDoubtToken is ERC20 {
         }
         return (0, false);
     }
-    
+
+    /*
+     * constructor
+     */
+
     constructor (uint initialSupply) public {
         _totalSupply = initialSupply;
         _balances[msg.sender] = initialSupply;
     }
-    
+
+    /*
+     * Public Functions
+     */
+
     function registerJudgement () public {
         uint _;
         bool success;
-        (_, success) = index(judgemen,msg.sender);
+        (_, success) = index(judgemen, msg.sender);
         require(success == false);
         judgemen.push(msg.sender);
     }
-    
+
     function changeBalance (address _from, address _to, uint256 _amount) public {
         _subBalance(_from, _amount);
         _addBalance(_to, _amount);
     }
-    
+
+    /*
+     * Private Functions
+     */
+
     function _subBalance (address _owner, uint256 _amount) private {
         _balances[_owner] = _balances[_owner].sub(_amount);
     }
-    
-    function _addBalance (address _taisyo, uint256 _amount) private {
-        _balances[_taisyo] = _balances[_taisyo].add(_amount);
+
+    function _addBalance (address _target, uint256 _amount) private {
+        _balances[_target] = _balances[_target].add(_amount);
     }
 }
--- ToyCash.sol
+++ ToyCash.sol
@@ -2,39 +2,40 @@ pragma solidity ^0.4.25;
 
 import "./ECRecovery.sol";
 
-contract ToyCash{
-    
-    //function to get token balance
-    function getTokenBalance (address _token_address) public view returns(uint) {
-        ERC20Interface token = ERC20Interface(_token_address);
+contract ToyCash {
+
+    // function to get token balance
+    function getTokenBalance(address _tokenAddress) public view returns(uint) {
+        ERC20Interface token = ERC20Interface(_tokenAddress);
         return token.balanceOf(msg.sender);
     }
 
-    //list for keeping whether tweets are already registered
+    // list for keeping whether tweets are already registered
     mapping (uint256 => bool) public tweet_registered;
-    
-    //function to take in tweet into block
+
+    // function to take in tweet into block
     function setTweet(
-        uint256 _tweet_id,
-        address _user_address,
-        address _judge_address,
+        uint256 _tweetId,
+        address _userAddress,
+        address _judgeAddress,
         uint256 _amount,
-        address _token_address,
-        bytes32 _tweet_hash,
-        bytes _judge_sig
+        address _tokenAddress,
+        bytes32 _tweetHash,
+        bytes _judgeSig
     ) public {
-        ERC20Interface token = ERC20Interface(_token_address);
-        
-        //validate signature
-        require(_judge_address == ECRecovery.recover(_tweet_hash, _judge_sig));
-        
-        require(!tweet_registered[_tweet_id]);
-        
-        require(token.balanceOf(_judge_address) >= _amount);
-        
-        token.changeBalance(_judge_address, _user_address, _amount);
-        //keeping this tweet are already registered
-        tweet_registered[_tweet_id] = true;
+        ERC20Interface token = ERC20Interface(_tokenAddress);
+
+        // validate signature
+        require(_judgeAddress == ECRecovery.recover(_tweetHash, _judgeSig));
+
+        require(!tweet_registered[_tweetId]);
+
+        require(token.balanceOf(_judgeAddress) >= _amount);
+
+        token.changeBalance(_judgeAddress, _userAddress, _amount);
+
+        // keeping this tweet are already registered
+        tweet_registered[_tweetId] = true;
     }
 }
 
